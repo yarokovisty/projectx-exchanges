fd7c1fd3a701d32834213e026f509774271a8f07
Uniciti <krayuskhin_vlad@mail.ru>
2024-07-14 15:08:32 +0700

    added sorting functionality

--- components/Pairs.tsx
+++ components/Pairs.tsx
@@ -1,15 +1,39 @@
 import { IPriceData } from "./interfaces"
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 
-interface IPropsPairs { exchangesData: IPriceData[] }
+interface IPropsPairs {
+    exchangesData: IPriceData[];
+    sortData: (key: string) => void;
+    sortConfig: { key: string; direction: string };
+}
 
-export const Pairs: React.FC<IPropsPairs> = ({exchangesData}) => {
+export const Pairs: React.FC<IPropsPairs> = ({ exchangesData, sortData, sortConfig }) => {
+
+    const getSortIcon = (key: string) => {
+        if (sortConfig.key !== key) {
+            return <FaSort />;
+        }
+        if (sortConfig.direction === "ascending") {
+            return <FaSortUp />;
+        }
+        return <FaSortDown />;
+    };
+
     return <Table aria-label="TableSpread">
         <TableHeader>
-            <TableColumn className="text-lg font-bold">Биржа</TableColumn>
-            <TableColumn className="text-lg font-bold">Монеты</TableColumn>
-            <TableColumn className="text-lg font-bold">Покупка</TableColumn>
-            <TableColumn className="text-lg font-bold">Продажа</TableColumn>
+            <TableColumn className="text-lg font-bold" onClick={() => sortData("exchangeId")}>
+                Биржа {getSortIcon("exchangeId")}
+            </TableColumn>
+            <TableColumn className="text-lg font-bold" onClick={() => sortData("pair")}>
+                Монеты {getSortIcon("pair")}
+            </TableColumn>
+            <TableColumn className="text-lg font-bold" onClick={() => sortData("price.buy")}>
+                Покупка {getSortIcon("price.buy")}
+            </TableColumn>
+            <TableColumn className="text-lg font-bold" onClick={() => sortData("price.sell")}>
+                Продажа {getSortIcon("price.sell")}
+            </TableColumn>
             <TableColumn className="text-lg font-bold">Комиссии</TableColumn>
             <TableColumn className="text-lg font-bold">Статус</TableColumn>
         </TableHeader>
